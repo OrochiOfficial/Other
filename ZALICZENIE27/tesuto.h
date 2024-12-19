bc490e9c501681069e4e005093f2063551355382
OrochiOfficial <107553767+OrochiOfficial@users.noreply.github.com>
2024-12-19 20:08:36 +0100

    Add files via upload

---
+++ ZALICZENIE27/tesuto.h
@@ -0,0 +1,15 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <pgmspace.h>
+
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+  <html>
+    <body>
+        <h1>  Twoja stara  </h1>
+    </body>
+  </html
+)=====";
+
+#endif
